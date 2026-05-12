9282f47385fcddf0be0e64266dc5d8533ea3e0cd
Bryan Herrera <bryan.herrera72@gmail.com>
2026-05-12 11:37:55 -0700

    reviving the portfolio site. Made some compatability changes and began fixing vulnerabilities

--- src/components/IntroSection/AnimatedSection/AnimatedSection.js
+++ src/components/IntroSection/AnimatedSection/AnimatedSection.js
@@ -1,24 +1,30 @@
-import React from 'react';
-import {useSpring, animated} from 'react-spring';
-import './AnimatedSection.css';
-import TypingText from './TypingText/TypingText';
+import React from "react"
+import { useSpring, animated } from "react-spring"
+import "./AnimatedSection.css"
+import TypingText from "./TypingText/TypingText"
 
+const calc = (x, y) => [
+  -(y - window.innerHeight / 2) / 20,
+  (x - window.innerWidth / 2) / 50,
+  1.3,
+]
+const trans = (x, y, s) =>
+  `perspective(600px) rotateX(${x}deg) rotateY(${y}deg) scale(${s})`
 
-const calc = (x, y) => [-(y - window.innerHeight / 2) / 20, (x - window.innerWidth / 2) / 50, 1.3]
-const trans = (x, y, s) => `perspective(600px) rotateX(${x}deg) rotateY(${y}deg) scale(${s})`
-
-const animatedText = props =>{
-    
-    const [val, set] = useSpring(() => ({ xys: [0, 0, 1], config: { mass: 8, tension: 200, friction: 80 } }))
-    return (
-        <animated.div  
-            className = "AnimatedSection"
-            onMouseMove={({ clientX: x, clientY: y }) => set({ xys: calc(x, y) })}
-            onMouseLeave={() => set({ xys: [0, 0, 1] })}
-            style={{ transform: val.xys.interpolate(trans) }}>
-            
-            <TypingText/>
-        </animated.div>
-    );
+const AnimatedText = props => {
+  const [val, set] = useSpring(() => ({
+    xys: [0, 0, 1],
+    config: { mass: 8, tension: 200, friction: 80 },
+  }))
+  return (
+    <animated.div
+      className="AnimatedSection"
+      onMouseMove={({ clientX: x, clientY: y }) => set({ xys: calc(x, y) })}
+      onMouseLeave={() => set({ xys: [0, 0, 1] })}
+      style={{ transform: val.xys.interpolate(trans) }}
+    >
+      <TypingText />
+    </animated.div>
+  )
 }
-export default animatedText;
+export default AnimatedText
